5beb5adad84b8c981a2db2f3d120419dc09dfef1
luci-000 <bherfelismenia@gmail.com>
2022-07-08 19:03:30 +0800

    Add graph on revenue page

--- src/app/main-salesreport/main-salesreport.module.ts
+++ src/app/main-salesreport/main-salesreport.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MainSalesReportRoutingComponent } from './main-salesreport.routing.module';
 import { MaterialModule } from '../modules/material.modules';
+import { NgChartsModule } from 'ng2-charts';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RevenueComponent } from './revenue/revenue.component';
@@ -15,6 +16,11 @@ import { ReportComponent } from './report/report.component';
     GrowthComponent,
     ReportComponent,
   ],
-  imports: [CommonModule, MaterialModule, MainSalesReportRoutingComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    MainSalesReportRoutingComponent,
+    NgChartsModule,
+  ],
 })
 export class MainSalesReportModule {}
--- src/app/main-salesreport/revenue/revenue.component.ts
+++ src/app/main-salesreport/revenue/revenue.component.ts
@@ -1,15 +1,158 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
+import { default as Annotation } from 'chartjs-plugin-annotation';
 @Component({
   selector: 'app-revenue',
   templateUrl: './revenue.component.html',
-  styleUrls: ['./revenue.component.css']
+  styleUrls: ['./revenue.component.css'],
 })
 export class RevenueComponent implements OnInit {
+  header = [
+    {
+      text: 'Today’s Sales',
+    },
+    {
+      text: 'Monthly Gross Sales',
+    },
+    {
+      text: 'Today’s Customers',
+    },
+    {
+      text: 'Monthly Purchases',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    Chart.register(Annotation);
   }
 
+  ngOnInit(): void {}
+
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40, 34, 77, 23, 99, 64],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90, 34, 767, 88, 33, 76],
+        label: 'Series B',
+        backgroundColor: 'rgba(77,83,96,0.2)',
+        borderColor: 'rgba(77,83,96,1)',
+        pointBackgroundColor: 'rgba(77,83,96,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(77,83,96,1)',
+        fill: 'origin',
+      },
+      {
+        data: [180, 480, 770, 90, 1000, 270, 400, 234, 344, 664, 767, 675],
+        label: 'Series C',
+        yAxisID: 'y-axis-1',
+        backgroundColor: 'rgba(206,146,223,0.6)',
+        borderColor: 'violet',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+    ],
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5,
+      },
+    },
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      x: {},
+      'y-axis-0': {
+        position: 'left',
+      },
+      'y-axis-1': {
+        position: 'right',
+        grid: {
+          color: 'rgba(255,0,0,0.3)',
+        },
+        ticks: {
+          color: 'red',
+        },
+        // to hide second axis
+        display: false,
+      },
+    },
+
+    // plugins: {
+    //   legend: { display: true },
+    //   annotation: {
+    //     annotations: [
+    //       {
+    //         type: 'line',
+    //         scaleID: 'x',
+    //         value: 'March',
+    //         borderColor: 'orange',
+    //         borderWidth: 2,
+    //         label: {
+    //           position: 'center',
+    //           enabled: true,
+    //           color: 'orange',
+    //           content: 'LineAnno',
+    //           font: {
+    //             weight: 'bold',
+    //           },
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
+  };
+
+  public lineChartType: ChartType = 'line';
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
 }
