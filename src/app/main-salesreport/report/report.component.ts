7a90d7e415dc4602190e8df9f0be4a172e39ae3e
luci-000 <bherfelismenia@gmail.com>
2022-07-09 18:06:57 +0800

    Finish report page base design

--- src/app/main-salesreport/report/report.component.ts
+++ src/app/main-salesreport/report/report.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+  styleUrls: ['./report.component.css'],
 })
 export class ReportComponent implements OnInit {
+  monthsLabel = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
