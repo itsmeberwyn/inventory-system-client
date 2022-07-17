7dba88a38a6ab91ba558d346380dbbed7832711d
luci-000 <bherfelismenia@gmail.com>
2022-07-17 16:41:40 +0800

    Adjust styling converting px to vh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,17 +22,6 @@ import { LoginSalesreportComponent } from './login-salesreport/login-salesreport
 import { SidebarComponent } from './reusable/sidebar/sidebar.component';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
-import { AddSupplierComponent } from './modals/supplier/add-supplier/add-supplier.component';
-import { AddPurchaseComponent } from './modals/purchase/add-purchase/add-purchase.component';
-import { DeletePurchaseComponent } from './modals/purchase/delete-purchase/delete-purchase.component';
-import { EditPurchaseComponent } from './modals/purchase/edit-purchase/edit-purchase.component';
-import { ViewPurchaseComponent } from './modals/purchase/view-purchase/view-purchase.component';
-import { DeleteProductComponent } from './modals/product/delete-product/delete-product.component';
-import { EditProductComponent } from './modals/product/edit-product/edit-product.component';
-import { AddProductComponent } from './modals/product/add-product/add-product.component';
-import { EditOrderComponent } from './modals/order/edit-order/edit-order.component';
-import { DeleteOrderComponent } from './modals/order/delete-order/delete-order.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,16 +32,6 @@ import { DeleteOrderComponent } from './modals/order/delete-order/delete-order.c
     SidebarComponent,
     LoginPointofsaleComponent,
     LoginSalesreportComponent,
-    AddSupplierComponent,
-    AddPurchaseComponent,
-    DeletePurchaseComponent,
-    EditPurchaseComponent,
-    ViewPurchaseComponent,
-    DeleteProductComponent,
-    EditProductComponent,
-    AddProductComponent,
-    EditOrderComponent,
-    DeleteOrderComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/main-inventory/main-inventory.module.ts
+++ src/app/main-inventory/main-inventory.module.ts
@@ -9,9 +9,19 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent as InventoryProductsComponent } from './products/products.component';
 import { PurchasesComponent } from './purchases/purchases.component';
 import { SuppliersComponent } from './suppliers/suppliers.component';
+
 import { DeleteSupplierComponent } from '../modals/supplier/delete-supplier/delete-supplier.component';
 import { EditSupplierComponent } from '../modals/supplier/edit-supplier/edit-supplier.component';
+import { AddSupplierComponent } from './../modals/supplier/add-supplier/add-supplier.component';
 
+import { AddPurchaseComponent } from '../modals/purchase/add-purchase/add-purchase.component';
+import { DeletePurchaseComponent } from './../modals/purchase/delete-purchase/delete-purchase.component';
+import { ViewPurchaseComponent } from './../modals/purchase/view-purchase/view-purchase.component';
+import { EditPurchaseComponent } from './../modals/purchase/edit-purchase/edit-purchase.component';
+
+import { DeleteProductComponent } from './../modals/product/delete-product/delete-product.component';
+import { AddProductComponent } from './../modals/product/add-product/add-product.component';
+import { EditProductComponent } from './../modals/product/edit-product/edit-product.component';
 @NgModule({
   declarations: [
     HomeComponent,
@@ -20,6 +30,14 @@ import { EditSupplierComponent } from '../modals/supplier/edit-supplier/edit-sup
     SuppliersComponent,
     EditSupplierComponent,
     DeleteSupplierComponent,
+    AddSupplierComponent,
+    AddPurchaseComponent,
+    DeletePurchaseComponent,
+    ViewPurchaseComponent,
+    EditPurchaseComponent,
+    DeleteProductComponent,
+    AddProductComponent,
+    EditProductComponent,
   ],
   imports: [
     CommonModule,
